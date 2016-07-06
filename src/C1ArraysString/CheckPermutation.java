4f3f168cb0e090d03e14638f35902be4b57301a0
Abhishek Gupta <abhi23gupta@gmail.com>
2016-07-06 00:47:57 -0700

    Check Permutation 1.2

---
+++ src/C1ArraysString/CheckPermutation.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package C1ArraysString;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author gupta.abh
+ */
+
+//Give Two String Write a Method to decide if one is permutation of the other
+
+// Algorithm
+/*
+Step 1 - Check if both string are equal in length 
+Step 2 If Equal then Sort Both the string
+Step 3 If sorted Strings are equal then return true
+
+*/
+
+
+public class CheckPermutation {
+    
+    public static void main(String[] args){
+        if(isPermutation("nj u ", "uj np")==true){
+            System.out.println("Both String are permutation of each other");
+            
+        }else{
+            System.out.println("No permutation of each other");
+        }
+        
+    }
+    
+    public static boolean isPermutation(String first, String second){
+        boolean ispermutation = false;
+        if(first.length()==second.length()){
+            first = sortString(first);
+            second = sortString(second);
+            if(first.equals(second)){
+                ispermutation = true;
+            }else{
+                ispermutation = false;
+            }
+            
+        }else{
+            
+            ispermutation = false;
+        }
+        return ispermutation;
+    }
+    
+    public static String sortString(String s){
+        char[] str = s.toCharArray();
+        Arrays.sort(str);
+         String sorted = new String(str);
+        return sorted;
+    }
+    
+}
--- src/C1ArraysString/IsUniqueString.java
+++ src/C1ArraysString/IsUniqueString.java
@@ -12,40 +12,42 @@ import java.util.Scanner;
  * @author gupta.abh
  */
 public class IsUniqueString {
-    
+
     //ASCII table has 128 symbols  A !=a
     //Extended ASCII table has 256 symbols  A!=a
     //Time complexity O(s.length())
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your String");
-        
-        String s = sc.next();        
-        if(isUnique(s)==true){
+
+        String s = sc.next();
+        if (isUnique(s) == true) {
             System.out.println("This is not a Unique String");
-        }else{
+        } else {
             System.out.println("This is a unique String");
         }
-        
+
     }
 
-    
-    public static boolean  isUnique(String s){
-    boolean[] Array = new boolean[128];
-    boolean isfound = false;
-    for(int i=0;i<s.length();i++){
-        if(Array[s.charAt(i)]==true){
+    public static boolean isUnique(String s) {
+        boolean[] Array = new boolean[128];
+        boolean isfound = false;
+        if (s.length() > 256) {
             isfound = true;
-            break;
-        }else{
-            Array[s.charAt(i)] = true;
+        } else {
+            for (int i = 0; i < s.length(); i++) {
+                if (Array[s.charAt(i)] == true) {
+                    isfound = true;
+                    break;
+                } else {
+                    Array[s.charAt(i)] = true;
+                }
+            }
         }
+
+        
+        return isfound;
     }
-    return isfound;
-}
-    
-    
-    
-    
+
 }
